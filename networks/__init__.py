79705da182241aa792d8a3c3316e23e8630051f1
MSHZ <101371071+Mona-ShZeinoddin@users.noreply.github.com>
2025-02-16 15:09:35 +0000

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -5,3 +5,4 @@ from .pose_cnn import PoseCNN
 from .appearance_flow_decoder import TransformDecoder
 from .optical_flow_decoder import PositionDecoder
 from .endo_fast3r_depth import Endo_FASt3r_depth
+from .endo_fast3r_pose import Endo_FASt3r_pose
