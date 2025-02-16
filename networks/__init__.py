f6dc74fc7e69470539edb9f3c5203b560e202077
MSHZ <101371071+Mona-ShZeinoddin@users.noreply.github.com>
2025-02-16 14:33:16 +0000

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -4,12 +4,4 @@ from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .appearance_flow_decoder import TransformDecoder
 from .optical_flow_decoder import PositionDecoder
-# from .custom_DAM import Customised_DAM
-#from .custom_DAM_separatedLoRAinit import Customised_DAM
-from .custom_DAM_separated_LoRA_MoRA import Customised_DAM
-from .dino_encoder_lora import DINOEncoder
-# from .dino_encoder_lora import DINOEncoder
-# from .DAM import Customised_DAM
-from .modora import Customised_MoRA_DAM
-# from .modora_convnext import Customised_MoRA_DAM
-from .intrinsics_decoder import IntrinsicsHead
+from .endo_fast3r_depth import Endo_FASt3r_depth
