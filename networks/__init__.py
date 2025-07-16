559e366fea5e62fcf4f693a7e55d393b597f075a
MSHZ <101371071+Mona-ShZeinoddin@users.noreply.github.com>
2025-07-16 17:32:38 +0100

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -3,4 +3,4 @@ from .appearance_flow_decoder import TransformDecoder
 from .optical_flow_decoder import PositionDecoder
 from .endo_fast3r_depth import Endo_FASt3r_depth
 # from .endo_fast3r_pose import Endo_FASt3r_pose
-from .endo_fast3r_pose import build_reloc3r_model
+from .endo_fast3r_pose import Reloc3rX
