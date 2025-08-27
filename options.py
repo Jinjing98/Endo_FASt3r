71936c2406040d19198b4101f90ae7129d5dc36c
jinjing98 <xjj892290531@163.com>
2025-08-28 00:19:38 +0200

    arg_parse: parse_notebook version

--- options.py
+++ options.py
@@ -233,3 +233,8 @@ class MonodepthOptions:
     def parse(self):
         self.options = self.parser.parse_args()
         return self.options
+    
+    def parse_notebook(self, args):
+        self.options = self.parser.parse_args(args)
+        return self.options
+
