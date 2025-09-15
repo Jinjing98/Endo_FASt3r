d7c72f5a5dc3c56a1fe179cd2d77399954011c2c
jinjing98 <xjj892290531@163.com>
2025-09-15 13:19:41 +0200

    init copy

---
+++ posetr/marepo_transformer/transformer.py
@@ -0,0 +1,437 @@
+# Copyright © Niantic, Inc. 2024.
+import copy
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from .linear_attention import LinearAttention, FullAttention
+from einops.einops import rearrange, repeat
+# from transformer.position_encoding import PositionEncodingSine
+from .position_encoding import PositionEncodingSine
+from geoaware_pnet.utils import rotation_6d_to_matrix
+import importlib.util
+if importlib.util.find_spec('flash_attn'):
+    from flash_attn import flash_attn_func
+
+RANDOM_RESCALE_RATIO=1 # Hyperparam that control the percentage we use random recale? Default is set to 1
+import random
+random.seed(2024)
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, nhead, attention='linear'):
+        '''
+        attention: (optional) 'linear': linear attention like LofTR
+                              'flash': flash attention (only support new arch. such as A100)
+        '''
+        super().__init__()
+        # flag for whether to return the attention features for plotting
+        self.return_attention=False
+        self.dim = d_model // nhead # 512/8
+        self.nhead = nhead # 8
+
+        # multi-head attention
+        self.q_proj = nn.Linear(d_model, d_model, bias=False)
+        self.k_proj = nn.Linear(d_model, d_model, bias=False)
+        self.v_proj = nn.Linear(d_model, d_model, bias=False)
+
+        self.enable_flash=False
+        if attention=='linear':
+            self.attention = LinearAttention()
+        elif attention=='flash':
+            self.attention = flash_attn_func
+            self.enable_flash=True
+        else:
+            self.attention = FullAttention()
+        self.merge = nn.Linear(d_model, d_model, bias=False)
+
+        # feed-forward network/FFN
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model*2, d_model*2, bias=False),
+            nn.ReLU(True),
+            nn.Linear(d_model*2, d_model, bias=False),
+        )
+
+        # norm and dropout
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+    def forward(self, x, source, x_mask=None, source_mask=None, return_attn=False):
+        """
+        Args:
+            x (torch.Tensor): [N, L, C]
+            source (torch.Tensor): [N, S, C]
+            x_mask (torch.Tensor): [N, L] (optional)
+            source_mask (torch.Tensor): [N, S] (optional)
+        """
+        bs = x.size(0)
+        query, key, value = x, source, source
+
+        # multi-head attention
+        query = self.q_proj(query).view(bs, -1, self.nhead, self.dim)  # [N, L, (H, D)], [32, 4800, 4, 32]
+        key = self.k_proj(key).view(bs, -1, self.nhead, self.dim)  # [N, S, (H, D)], [32, 4800, 4, 32]
+        value = self.v_proj(value).view(bs, -1, self.nhead, self.dim) # [32, 4800, 4, 32]
+
+        if return_attn == True:
+            attn = self.attention.get_QK_attention(query, key, value, q_mask=x_mask, kv_mask=source_mask)
+            return attn
+        if self.enable_flash:
+            # flash attention only supports fp16 or bf16
+            attn = self.attention(query, key, value) # flash attention currently does not support mask https://github.com/Dao-AILab/flash-attention/issues/409
+        else:
+            attn = self.attention(query, key, value, q_mask=x_mask, kv_mask=source_mask) #.type(x.dtype)  # [N, L, (H, D)], use in fp32?
+
+        message = self.merge(attn.view(bs, -1, self.nhead*self.dim))  # [N, L, C]
+        message = self.norm1(message)#.type(x.dtype) # keep float16 if needed
+
+        # feed-forward network
+        message = self.mlp(torch.cat([x, message], dim=2))
+        message = self.norm2(message)#.type(x.dtype) # keep float16 if needed
+        return x + message
+ 
+class ResConvBlock(nn.Module):
+    '''
+    1x1 convolution residual block
+    '''
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        # We may need a skip layer if the number of features output by the encoder is different.
+        self.head_skip = nn.Identity() if self.in_channels == self.out_channels else nn.Conv2d(self.in_channels, self.out_channels, 1, 1, 0)
+        self.res_conv1 = nn.Conv2d(self.in_channels, self.out_channels, 1, 1, 0)
+        self.res_conv2 = nn.Conv2d(self.out_channels, self.out_channels, 1, 1, 0)
+        self.res_conv3 = nn.Conv2d(self.out_channels, self.out_channels, 1, 1, 0)
+
+    def forward(self, res):
+        x = F.relu(self.res_conv1(res))
+        x = F.relu(self.res_conv2(x))
+        x = F.relu(self.res_conv3(x))
+        res = self.head_skip(res) + x
+        return res
+
+class Transformer_Head(nn.Module):
+    """
+    A Transformer Pose Regression Head.
+    The network predicts a 6DOF pose from given 3d scene coordinate
+    """
+    def __init__(self, config):
+        """
+        input_dim: is the size of input HxW
+        """
+        super().__init__()
+        # Misc
+        self.config = config
+        self.d_model = self.config['d_model']
+        self.px_resample_rule_dict_scale_step = self.config['px_resample_rule_dict_scale_step']
+
+        self.use_homogeneous = False
+        if 'use_homogeneous_pose_t' in self.config:
+            if self.config['use_homogeneous_pose_t'] == 'True':
+                self.use_homogeneous = True,
+                homogeneous_min_scale = 0.01,
+                homogeneous_max_scale = 4.0,
+
+        # positional encoding
+        self.pos_encoding = PositionEncodingSine(self.config, in_ch_dim=3, sc_pe=self.config["pre_pos_encoding"], pixel_pe=self.config["pixel_pos_encoding"])
+
+        # section 3.3 of paper
+        self.coarse_to_fine_prediction=False
+        if 'c2f' in self.config:
+            self.coarse_to_fine_prediction=True if config['c2f']=='V0' else NotImplementedError
+
+        # currently we only support '6D' and '9D' rotation representation
+        self.rot_representation = self.config['rotation_representation'] if 'rotation_representation' in self.config else '6D'
+
+        ''' Large scene exp. consider using this with large scale scenes'''
+        # These parameters are only useful if random_rescale_sc=True at forward()
+        self.random_rescale_max = config['random_rescale_max'] if 'random_rescale_max' in config else 10
+        self.random_shift_max = config['random_shift_max'] if 'random_shift_max' in config else 0
+
+        # transformer + pose regression part
+        self.res_conv = nn.ModuleList([copy.deepcopy(ResConvBlock(self.d_model, self.d_model))
+                                       for _ in range(self.config["num_resconv_block"])])
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+
+        if self.use_homogeneous:
+            self.fc_t = nn.Linear(self.d_model, 4)
+            # Use buffers because they need to be saved in the state dict.
+            self.register_buffer("max_scale", torch.tensor([homogeneous_max_scale]))
+            self.register_buffer("min_scale", torch.tensor([homogeneous_min_scale]))
+            self.register_buffer("max_inv_scale", 1. / self.max_scale)
+            self.register_buffer("h_beta", math.log(2) / (1. - self.max_inv_scale))
+            self.register_buffer("min_inv_scale", 1. / self.min_scale)
+        else:
+            self.fc_t = nn.Linear(self.d_model, 3)
+
+        if self.rot_representation=='9D':
+            self.fc_rot = nn.Linear(self.d_model, 9)
+        else:
+            self.fc_rot = nn.Linear(self.d_model, 6)
+        # Learn scene coordinates relative to a mean coordinate (e.g. center of the scene).
+        self.register_buffer("transformer_pose_mean", self.config['transformer_pose_mean'].clone().detach().view(1, 3, 1, 1)) # torch.Size([1, 3, 1, 1])
+
+        self.more_mlps = nn.Sequential(
+            nn.Linear(self.d_model,self.d_model),
+            nn.ReLU(),
+            nn.Linear(self.d_model,self.d_model),
+            nn.ReLU()
+            )
+
+    def pose_regression_head(self, feat, sc_shape=[], scale_factor=None, translation_sc_shift_offset=None):
+        """
+        This is my implementation on pose regression head
+        """
+        B, C, H, W = sc_shape
+        # reduce feature dimension
+        feat = feat.view(B, H, W, self.d_model).permute(0, 3, 1, 2).contiguous()  # [N,C,H,W]
+
+        for i in range(self.config["num_resconv_block"]):
+            feat = self.res_conv[i](feat)
+        feat = self.avgpool(feat)  # [N, 256, 1, 1]
+        feat = feat.view(feat.size(0), -1)
+
+        feat = self.more_mlps(feat)
+
+        # predict absolute pose
+        out_t = self.fc_t(feat)  # [N,3]
+
+        if self.use_homogeneous:
+            # Dehomogenize coords:
+            # Softplus ensures we have a smooth homogeneous parameter with a minimum value = self.max_inv_scale.
+            h_slice = F.softplus(out_t[:, 3].unsqueeze(1), beta=self.h_beta.item()) + self.max_inv_scale
+            h_slice.clamp_(max=self.min_inv_scale)
+            out_t = out_t[:, :3] / h_slice
+
+        # add mapping mean back
+        # DO Not remove, used in testing, precision is fp32 from this point
+        out_t = out_t.float() + self.transformer_pose_mean.clone().detach().view(3)
+        out_r = self.fc_rot(feat)  # [N,6]
+        return out_t, out_r
+
+    def svd_orthogonalize(self, m):
+        """Convert 9D representation to SO(3) using SVD orthogonalization.
+
+        Args:
+          m: [BATCH, 3, 3] 3x3 matrices.
+
+        Returns:
+          [BATCH, 3, 3] SO(3) rotation matrices.
+        """
+        if m.dim() < 3:
+            m = m.reshape((-1, 3, 3))
+        m_transpose = torch.transpose(torch.nn.functional.normalize(m, p=2, dim=-1), dim0=-1, dim1=-2)
+        u, s, v = torch.svd(m_transpose)
+        det = torch.det(torch.matmul(v, u.transpose(-2, -1)))
+        # Check orientation reflection.
+        r = torch.matmul(
+            torch.cat([v[:, :, :-1], v[:, :, -1:] * det.view(-1, 1, 1)], dim=2),
+            u.transpose(-2, -1)
+        )
+        return r
+
+    def rot_from_axisangle(self, vec):
+        """Convert an axisangle rotation into a 4x4 transformation matrix
+        (adapted from https://github.com/Wallacoloo/printipi)
+        Input 'vec' has to be Bx1x3
+        """
+        angle = torch.norm(vec, 2, 2, True)
+        axis = vec / (angle + 1e-7)
+
+        ca = torch.cos(angle)
+        sa = torch.sin(angle)
+        C = 1 - ca
+
+        x = axis[..., 0].unsqueeze(1)
+        y = axis[..., 1].unsqueeze(1)
+        z = axis[..., 2].unsqueeze(1)
+
+        xs = x * sa
+        ys = y * sa
+        zs = z * sa
+        xC = x * C
+        yC = y * C
+        zC = z * C
+        xyC = x * yC
+        yzC = y * zC
+        zxC = z * xC
+
+        rot = torch.zeros((vec.shape[0], 4, 4)).to(device=vec.device)
+
+        rot[:, 0, 0] = torch.squeeze(x * xC + ca)
+        rot[:, 0, 1] = torch.squeeze(xyC - zs)
+        rot[:, 0, 2] = torch.squeeze(zxC + ys)
+        rot[:, 1, 0] = torch.squeeze(xyC + zs)
+        rot[:, 1, 1] = torch.squeeze(y * yC + ca)
+        rot[:, 1, 2] = torch.squeeze(yzC - xs)
+        rot[:, 2, 0] = torch.squeeze(zxC - ys)
+        rot[:, 2, 1] = torch.squeeze(yzC + xs)
+        rot[:, 2, 2] = torch.squeeze(z * zC + ca)
+        rot[:, 3, 3] = 1
+
+        return rot
+
+    def transformation_from_parameters(self, axisangle, translation, invert=False):
+        """Convert the network's (axisangle, translation) output into a 4x4 matrix
+        """
+        R = self.rot_from_axisangle(axisangle)
+        t = translation.clone()
+
+        if invert:
+            R = R.transpose(1, 2)
+            t *= -1
+
+        T = self.get_translation_matrix(t)
+
+        if invert:
+            M = torch.matmul(R, T)
+        else:
+            M = torch.matmul(T, R)
+
+        return M
+    
+    def get_translation_matrix(self, translation_vector):
+        """Convert a translation vector into a 4x4 transformation matrix
+        """
+        T = torch.zeros(translation_vector.shape[0], 4, 4).to(device=translation_vector.device)
+
+        t = translation_vector.contiguous().view(-1, 3, 1)
+
+        T[:, 0, 0] = 1
+        T[:, 1, 1] = 1
+        T[:, 2, 2] = 1
+        T[:, 3, 3] = 1
+        T[:, :3, 3, None] = t
+
+        return T
+
+    def convert_pose_to_4x4(self, B, out_r, out_t, device, addtional_scale=0.001):
+        out_r = addtional_scale * out_r
+        out_t = addtional_scale * out_t
+
+
+        # if self.rot_representation=='9D':
+        #     out_r = self.svd_orthogonalize(out_r)  # [N,3,3]
+        # else:
+        #     out_r = rotation_6d_to_matrix(out_r)  # [N,3,3]
+        # pose = torch.zeros((B, 4, 4), device=device)
+        # pose[:, :3, :3] = out_r
+        # pose[:, :3, 3] = out_t
+        # pose[:, 3, 3] = 1.
+        # return pose
+        pose = self.transformation_from_parameters(out_r.unsqueeze(1), out_t)
+        return pose
+    
+    def convert_pose_to_4x4_ori(self, B, out_r, out_t, device, addtional_scale=1.0):
+        # out_r = addtional_scale * out_r
+        # out_t = addtional_scale * out_t
+
+        if self.rot_representation=='9D':
+            out_r = self.svd_orthogonalize(out_r)  # [N,3,3]
+        else:
+            out_r = rotation_6d_to_matrix(out_r)  # [N,3,3]
+        pose = torch.zeros((B, 4, 4), device=device)
+        pose[:, :3, :3] = out_r
+        pose[:, :3, 3] = out_t
+        pose[:, 3, 3] = 1.
+        return pose
+
+    def runtime_augmentation(self, sc_coords, B, C, random_rescale_sc):
+        '''
+        add random_rescale_sc augmentation during training
+        '''
+        if random.uniform(0, 1) < RANDOM_RESCALE_RATIO:  # only use in training
+            # basically, this is a random data augmentation on scene coordinates, to cope with large scale dataset like Cambridge
+            # randomly generate dilation scale random numbers. Inspired by erosion/dilation in CV.
+            lower_dilation_bound = 1
+            upper_dilation_bound = self.random_rescale_max
+            # upper_dilation_bound = 5
+            # uniformly generate factor between [1~5]
+            scale_factor = (upper_dilation_bound - lower_dilation_bound) * torch.rand((B, 1, 1, 1),
+                                                                                         device=sc_coords.device) + lower_dilation_bound
+            sc_coords_new = sc_coords * scale_factor
+            scale_factor = scale_factor[:, :, 0, 0]  # [B,1,1,1] -> [B,1]
+
+            # randomly generate shift scene coordinates to train the pose regressor for outputting large camera poses
+            upper_shift_bound = self.random_shift_max
+            # upper_shift_bound = 100 # uniformly generate 100 meter shift on 3 translational axis
+            translation_sc_shift_offset = 2 * upper_shift_bound * torch.rand((B, C, 1, 1),
+                                                                             device=sc_coords.device) - upper_shift_bound  # values range [-100 ~ 100]
+            sc_coords_new = sc_coords_new + translation_sc_shift_offset
+            translation_sc_shift_offset = translation_sc_shift_offset[:, :, 0, 0]  # [B,C,1,1] -> [B,C]
+        else:
+            return sc_coords, None, None
+
+        return sc_coords_new, scale_factor, translation_sc_shift_offset
+
+    def forward(self, sc, intrinsics_B33=None, sc_mask=None, random_rescale_sc=False, sample_level = 3):
+        """
+        Parameters:
+            sc: scene coordinate map [N,C,H,W]
+            intrinsics_B33: 3x3 camera intrinsics [B,3,3]
+            sc_mask: valid mask for sc [N,1,H,W]. True is valid, False is invalid
+            random_rescale_sc: we only add this config for large scene model
+        return:
+            pose [N,4,4]
+        """
+        # print('//////////////')
+        # print(sc.shape)
+        # print(intrinsics_B33.shape)
+        B,C,H,W = sc.shape
+
+        # subtract mapping mean
+        sc_coords = sc - self.transformer_pose_mean # DO Not remove, used in testing
+
+        sample_step = self.px_resample_rule_dict_scale_step[sample_level]
+        # print('for smaple level {}, sample_step: {}'.format(sample_level, sample_step))
+
+        # SC map positional encoding
+        sc_feat, pixel_pe = self.pos_encoding(sc_coords, intrinsics_B33, sample_step)
+        feat = rearrange(sc_feat, 'n c h w -> n (h w) c') # [32, 4800, 128]
+
+        if sc_mask==None:
+            mask_c0 = mask_c1 = None  # mask is useful in training
+        else:
+            sc_mask = sc_mask[:,0].view(B,H*W)
+            mask_c0 = mask_c1 = sc_mask
+
+        addtional_scale = 0.001
+        if self.coarse_to_fine_prediction:
+            feat_list = self.transformer(feat, None, mask_c0, None)
+            pose_list = []
+            for block_idx, feat in enumerate(feat_list):
+                out_t, out_r = self.pose_regression_head(feat, [B, C, H, W])
+                pose = self.convert_pose_to_4x4(B, out_r, out_t, sc_coords.device, addtional_scale)
+                pose_list.append(pose)
+            return pose_list
+        else:
+            feat = self.transformer(feat, None, mask_c0, None) # [N, HW, C] [64, 4800, 128]
+        out_t, out_r = self.pose_regression_head(feat, [B,C,H,W])
+
+        # convert rotation to SO(3), precision is fp32 from this point
+        pose = self.convert_pose_to_4x4(B, out_r, out_t, sc_coords.device, addtional_scale)
+        return pose
+
+if __name__ == "__main__":
+    # test transoformer_head with sample input
+    config = {
+        'd_model': 128,
+        'px_resample_rule_dict_scale_step': {3: 8},
+        'use_homogeneous_pose_t': 'True',
+        'rotation_representation': '6D',
+        'random_rescale_max': 10,
+        'random_shift_max': 0,
+        'num_resconv_block': 4,
+        'transformer_pose_mean': torch.randn(1, 3, 1, 1)
+    }
+
+    transformer_head = Transformer_Head(config)
+    input_sc = torch.randn(1, 3, 256, 256)
+    input_intrinsics = torch.randn(1, 3, 3)
+    input_sc_mask = torch.ones(1, 1, 256, 256)
+    input_random_rescale_sc = False
+    input_sample_level = 3
+    pose = transformer_head(
+        input_sc, input_intrinsics, input_sc_mask, input_random_rescale_sc, input_sample_level)
+    print(pose.shape)
+
+
